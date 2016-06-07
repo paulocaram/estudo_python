716479d5f010e4467866a2af9469063592ffeb98
root <paulo@wayinfotec.net.br>
2016-06-07 20:06:25 -0300

    atualiza autentica03.py

--- autentica03.py
+++ autentica03.py
@@ -21,5 +21,19 @@ def mensagem(auth):
     else:
         print "Acesso negado !!!"
 
+def menu():
+    print "\
+        1. Cadastra usuario\
+        2. Acessa sistema\
+        3. Sai do Sistema\n"
+
+    opcao = input("Digite sua opcao: ")
+
+    return opcao
+
+def switch(x):
+    dict_options = {1:cadastra,2:acessa,3:sai}
+    dict_options[x]();
+
 if __name__ == '__main__':
     mensagem(autentica())
