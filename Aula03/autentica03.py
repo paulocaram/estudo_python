f40992c5e6d909cf75e700f05c78db17bc108381
root <paulo@wayinfotec.net.br>
2016-06-07 21:11:37 -0300

    atualizando autentica03

--- Aula03/autentica03.py
+++ Aula03/autentica03.py
@@ -1,30 +1,43 @@
 #!/usr/bin/python
+#Importa o modulo sys
+import sys
 
 lista = []
 
-def autentica():
-    dict_lista = {"usuario":"","password":""}
-    dict_lista["usuario"] = raw_input("Digite o Login: ")
-    dict_lista["password"] = raw_input("Digite a senha: ")
+def sai():
+    print "Saindo do sistema !!"
+    sys.exit()
+
+def cadastra():
+    dict_lista = {"login":"","senha":""}
+    dict_lista["login"] = raw_input("Digite o login do novo usuario: ")    
+    dict_lista["senha"] = raw_input("Digite a nova senha: ")
     lista.append(dict_lista)
 
-    if (lista[0]["usuario"] == "paulo") and (lista[0]["password"] == "paulo"):
-        autenticado = True
+def acessa():
+    global lista
+    usuario = raw_input("Digite o login: ")
+    passwd = raw_input("Digite a senha: ")
+
+    if (lista[0]["login"] == usuario):
+    
+        if (lista[0]["senha"] == passwd):
+            mensagem(True,"Acesso permitido")
+        else:
+            mensagem(False,"Senha invalida")
     else:
-        autenticado = False
+        mensagem(False,"Acesso negado !!")
 
-    return autenticado 
-
-def mensagem(auth):
+def mensagem(auth,msg):
     if auth:
-        print "Acesso permitido !!!"
+        print msg
     else:
-        print "Acesso negado !!!"
+        print msg
 
 def menu():
     print "\
-        1. Cadastra usuario\
-        2. Acessa sistema\
+        1. Cadastra usuario\n\
+        2. Acessa sistema\n\
         3. Sai do Sistema\n"
 
     opcao = input("Digite sua opcao: ")
@@ -36,4 +49,6 @@ def switch(x):
     dict_options[x]();
 
 if __name__ == '__main__':
-    mensagem(autentica())
+
+    while True:
+        switch(menu())        
